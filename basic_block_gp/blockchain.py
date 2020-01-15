f134011f684896f230694a0c24847429533ad921
BuildTools <arthurpisakhov1@gmail.com>
2020-01-15 15:24:28 -0500

    new_block

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -30,14 +30,21 @@ class Blockchain(object):
         :return: <dict> New Block
         """
 
+        # block setup
         block = {
-            # TODO
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
 
         # Reset the current list of transactions
+        self.current_transactions = []
         # Append the chain to the block
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
     def hash(block):
         """
